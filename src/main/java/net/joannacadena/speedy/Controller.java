5029ae0716347ffb0807c55f87fd68b0db1a2daa
jocodyer <123603150+jocodyer@users.noreply.github.com>
2023-01-25 17:21:31 -0600

    added another endpoint

--- src/main/java/net/joannacadena/speedy/Controller.java
+++ src/main/java/net/joannacadena/speedy/Controller.java
@@ -1,10 +1,12 @@
 package net.joannacadena.speedy;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 
@@ -16,5 +18,9 @@ public class Controller {
     public ResponseEntity<String> greeting(@PathVariable String name){
 
         return ResponseEntity.ok("Hello "+name);
+    }
+    @GetMapping("/fail/{name}")
+    public ResponseEntity<String> fail(@PathVariable String name){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"You have not been found"+name);
     }
 }
